d83da2bff834c354bd8a143f7e63f077e3662e4a
Erikpapa <erikpapa01@gmail.com>
2026-09-17 20:25:45 -0300

    build: configure GitHub Pages deployment workflow
    Add GitHub Actions pipeline to automate build and deployment to Pages and update Vite base path for correct asset routing.

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/erik-site-teste/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
